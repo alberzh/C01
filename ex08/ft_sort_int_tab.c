5642e9780715979064265c916762a5405e6b5272
Alberto Zaragoza <alberzh@gmail.com>
2025-07-20 13:38:56 +0200

    Ejercicio ex08 listo.

--- ex08/ft_sort_int_tab.c
+++ ex08/ft_sort_int_tab.c
@@ -6,11 +6,59 @@
 /*   By: azaragoz <azaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 12:55:10 by azaragoz          #+#    #+#             */
-/*   Updated: 2025/07/20 12:55:51 by azaragoz         ###   ########.fr       */
+/*   Updated: 2025/07/20 13:38:41 by azaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void    ft_sort_int_tab(int *tab, int size)
 {
-    
+    int i;
+    int j;
+    int temp;
+
+    i = 0;
+    while (i < size - 1)
+    {
+        j = 0;
+        while (j < size - i - 1)
+        {
+            if (tab[j] > tab[j + 1])
+            {
+                temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+            }
+            j++;
+        }
+        i++;
+    }
 }
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int tab[] = {3, 9, 8, 1};
+    int size = 4;
+    int i;
+
+    printf("Array original: ");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+    
+    ft_sort_int_tab(tab, size);
+    
+    printf("Array ordenado: ");
+    for (i = 0; i < size; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+*/
